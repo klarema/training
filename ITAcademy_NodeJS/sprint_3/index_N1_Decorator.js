506d57f5354b6dc10c51e3fdbbcc341e81f97ff5
claremcc <mccarron.cl@gmail.com>
2022-05-08 19:11:07 +0200

    update decorator files

--- ITAcademy_NodeJS/sprint_3/index_N1_Decorator.js
+++ ITAcademy_NodeJS/sprint_3/index_N1_Decorator.js
@@ -1,20 +1,17 @@
-const calc = require('./calc');
+const {currencyKey, exchangeRate, calcuatePriceinEuro, calculateTotal} = require('./calc');
 const Product = require('./Product');
 
-var producto_1 = new Product("PC", 499.90, "USD");
-var producto_2 = new Product("Tablet", 299.90, "GBP");
+var pC = new Product("PC", 499.90, "USD");
+var tabletZ = new Product("Tablet", 299.90, "GBP");
+var iPhone = new Product("iPhone", 799.90, "CNY");
+var keyboard = new Product("keyboard", 89.90, "CHF");
+var monitor = new Product("monitor", 4550.90, "JPY");
+var tabletY = new Product("Tablet", 349.90, "CAD");
+var hPPrinter = new Product("HP Printer", 299.90, "CAD");
+shoppingList = [pC, tabletZ, iPhone, keyboard, monitor, tabletY, hPPrinter]
 
-// console.log(producto_1);
-// console.log(producto_2);
-
-// convert to Euro using the decorator
-// calc.calcCurrency(producto_1);
-calc.asyFunc2(producto_1);
-
-// console.log(producto_1);
-// console.log(producto_2);
-
-
+console.log(`\nGet total price for items in shopping cart: \n`)
+calculateTotal(shoppingList) 
 
 
 // DECORATOR - Crea un Decorator en un arxiu que retorni una funció. / create a decorator in a file that returns a function that ...
