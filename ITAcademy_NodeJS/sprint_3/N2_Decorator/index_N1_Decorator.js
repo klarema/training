619d83709f0630c411f4a9a974aaadbf6c578346
claremcc <mccarron.cl@gmail.com>
2022-05-28 23:32:43 +0200

    decorator pattern files v2

--- ITAcademy_NodeJS/sprint_3/N2_Decorator/index_N1_Decorator.js
+++ ITAcademy_NodeJS/sprint_3/N2_Decorator/index_N1_Decorator.js
@@ -1,21 +1,35 @@
-const {currencyKey, exchangeRate, calcuatePriceinEuro, calculateTotal} = require('./calc');
-const Product = require('./Product');
+const {
+    costCalc,
+    costCalc_USD,
+    costCalc_GBP,
+    costCalc_CHF,
+    costCalc_JPY,
+    costCalc_CAD,
+    costCalc_CNY,
+  } = require("./decorator");
+  const Product = require("./Product");
+  
+  var pC = new Product("PC", 499.9, "USD");
+  var tabletZ = new Product("Tablet", 299.9, "GBP");
+  var iPhone = new Product("iPhone", 799.9, "CNY");
+  var keyboard = new Product("keyboard", 89.9, "CHF");
+  var monitor = new Product("monitor", 4550.9, "JPY");
+  var tabletY = new Product("Tablet", 349.9, "CAD");
+  var hPPrinter = new Product("HP Printer", 299.9, "CAD");
+  
+  console.log("\n --- Calculation Total 1: --- ")
+  let totalCost = 0;
+  console.log("Total: " + (totalCost = totalCost + costCalc_USD(pC)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_USD(iPhone)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_USD(keyboard)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_USD(hPPrinter)).toFixed(2));
+  
+  console.log("\n --- Calculation Total 2: --- ")
+  totalCost = 0;
+  console.log("Total: " + (totalCost = totalCost + costCalc_JPY(tabletZ)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_JPY(tabletY)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_JPY(monitor)).toFixed(2));
+  console.log("Total: " + (totalCost = totalCost + costCalc_JPY(iPhone)).toFixed(2));
+  
 
-var pC = new Product("PC", 499.90, "USD");
-var tabletZ = new Product("Tablet", 299.90, "GBP");
-var iPhone = new Product("iPhone", 799.90, "CNY");
-var keyboard = new Product("keyboard", 89.90, "CHF");
-var monitor = new Product("monitor", 4550.90, "JPY");
-var tabletY = new Product("Tablet", 349.90, "CAD");
-var hPPrinter = new Product("HP Printer", 299.90, "CAD");
-shoppingList = [pC, tabletZ, iPhone, keyboard, monitor, tabletY, hPPrinter]
-
-console.log(`\nGet total price for items in shopping cart: \n`)
-calculateTotal(shoppingList) 
-
-
-// DECORATOR - Crea un Decorator en un arxiu que retorni una funció. / create a decorator in a file that returns a function that ...
-// Aquesta funció efectuarà una conversió de moneda a euros multiplicant pel coeficient de conversió del fitxer adjunt currency_conversions.json en funció de la divisa original / that does A CURRENCY conversion to EUR per the file attached
-// Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises incials, aplicant diferents conversions que usin el Decorator del punt anterior / create a small app that calculates the cost of some articles in euros starting with their initial currency, then apply a currency conversion per the previous points. 
-//parseFloat(items[i]);
-//totalPrice.toFixed(2)
+  
