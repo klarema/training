8692e381187b70afafc5bd13b61fb6073222c555
claremcc <mccarron.cl@gmail.com>
2022-12-29 23:34:06 +0100

    java kata

--- katas/java/firstClass/src/javaKata1/JavaKata1.java
+++ katas/java/firstClass/src/javaKata1/JavaKata1.java
@@ -1,29 +1,36 @@
 package javaKata1;
 import java.util.Random;
+import java.util.Scanner;
 
 public class JavaKata1 {
-    public static void main(String args[]){
-        int numberOfRolls = 5;
-        int advancedToSpace = 0;
-        int winsSpace = 20;
-        Random random = new Random();
-        for(int i = 0; i < numberOfRolls; i++){
-            int die = random.nextInt(6)+1;
-            advancedToSpace = advancedToSpace + die;
-            if(advancedToSpace < winsSpace){
-                System.out.println(i+1 +"- You rolled a "+ die + ". You are on space "
-                        + advancedToSpace + ". You need " + (winsSpace - advancedToSpace)
-                        + " more to win. ");
-            }
-            else break;
-        }
-        if(advancedToSpace == winsSpace){
-            System.out.println("You win!");
-        }
-        else if(advancedToSpace < winsSpace)
-        {
-            System.out.println("Game over! You were short by " + (winsSpace - advancedToSpace));
-        }
-        else System.out.println("Game over! You exceeds by " + (advancedToSpace - winsSpace));
+    static Scanner scanner = new Scanner(System.in);
+
+    public  static void main(String args[]) {
+        double basePlan = getBase();
+        int minutesOver = getOverage();
+        double overageFee = calculateOverage(minutesOver);
+        double taxAdded = calculateTax(overageFee);
+        calculateTotal(taxAdded, basePlan, overageFee);
+
+        scanner.close();
+    }
+    public static double getBase(){
+        System.out.println("Enter plan fee: ");
+        return scanner.nextDouble();
+    }
+    public static int getOverage(){
+        System.out.println("Enter plan overage in minutes: ");
+        return scanner.nextInt();
+    }
+    public static double calculateOverage(int minutesOver){
+        return minutesOver * 0.25;
+    }
+    public static double calculateTax(double overageFee){
+        return overageFee* 0.15;
+    }
+    public static void calculateTotal(double tax, double basePlan, double overageFee){
+        System.out.println("Total including overage fee("+overageFee+") and tax("+tax+
+                ") is: " + (String.format("%.2f", tax+overageFee+basePlan))
+        );
     }
 }
