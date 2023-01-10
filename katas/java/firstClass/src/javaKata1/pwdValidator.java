b607cb95790f096d772b533be8fed00241ea3cc0
claremcc <mccarron.cl@gmail.com>
2023-01-10 23:53:26 +0100

    java kata

--- katas/java/firstClass/src/javaKata1/pwdValidator.java
+++ katas/java/firstClass/src/javaKata1/pwdValidator.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class pwdValidator {
     public static void main(String args[]){
-        public static void main(String args[]){
-            String userName = "myname";
-            String oldPwd = "pa$$word";
-            String pwd1 = "PAsswo??";
-            String pwd2 = "mynamepass";
-            String pwd3 = "pa$$word";
-            boolean goodPassword = true;
+        String userName = "myName";
+        String oldPwd = "pa$$word";
+        String pwd1 = "PAsswo??";
+        String pwd2 = "myNamePass";
+        String pwd3 = "pa$$word";
+        boolean goodPassword = true;
 
-            if(pwd1.length()<8 || );
-            goodPassword = false;
-            // search for uppercase
-            // search for lowercase
-            // search for substring - username
-            // search for match with old password
+        do {
+            if(pwd1.length()< 8);
+                goodPassword = false;
+            if(pwd1.equalsIgnoreCase(oldPwd))
+                goodPassword = false;
+            }while(goodPassword);
+
         }
-    }
+
 }
