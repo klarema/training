fe8105a7cc9da5fb9b6068647456caa27bf9212d
claremcc <mccarron.cl@gmail.com>
2023-01-24 00:08:08 +0100

    java katas

--- katas/java/firstClass/src/javaKata1/Market.java
+++ katas/java/firstClass/src/javaKata1/Market.java
@@ -3,11 +3,15 @@ package javaKata1;
 public class Market {
     public static void main(String[]args){
         Fruit generic = new Fruit();
-        //Fruit banana1 = new Banana();
+        Fruit banana1 = new Banana(80);
         Banana banana2 = new Banana(70);
-        //Fruit apple1 = new Apple();
+        Fruit apple1 = new Apple();
         Apple apple2 = new Apple();
-        banana2.getCalories();
+        System.out.println("banana2:");
+        System.out.println(banana2.getCalories());
         banana2.makeJuice();
+        System.out.println("banana1:");
+        System.out.println(banana1.getCalories());
+        banana1.makeJuice();
     }
 }
