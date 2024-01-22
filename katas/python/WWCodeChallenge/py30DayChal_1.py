c1df6c3b5cc8351ee731918ec93e1553d59c72cb
claremcc <mccarron.cl@gmail.com>
2024-01-22 22:37:09 +0100

    day 4, 5

--- katas/python/WWCodeChallenge/py30DayChal_1.py
+++ katas/python/WWCodeChallenge/py30DayChal_1.py
@@ -15,11 +15,38 @@
 # print(calcCircleArea(radius))
 
 # day 3: Write a function to count the number of vowels in a given string
-myString = input("Enter name: ")
-def getVowelCount(myString):
-    count = 0
-    for x in myString:
-        if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
-            count += 1
-    return count
-print(getVowelCount(myString))
+# myString = input("Enter name: ")
+# def getVowelCount(myString):
+#     count = 0
+#     for x in myString:
+#         if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
+#             count += 1
+#     return count
+# print(getVowelCount(myString))
+
+# day 4: Write a program to find the sum of all elements in a list.
+mylist = ["apple", "orange", "banana", "cherry"]
+sum = 0
+for x in mylist:
+    sum += 1
+print(sum)
+
+# day 5: Write a program to count the occurrences of a specific character in a string.
+myString = "appleorangecherry"
+count = 0
+for x in myString:
+    if x == "r":
+        count += 1
+print(count)
+
+# day 6: Write a function to count the number of vowels in a given string
+
+# day 7: Write a program to check if a number is positive, negative, or zero.
+
+# day 8: Write a function that accepts a string and calculates the number of uppercase and lowercase letters in it
+
+# day 9: Write a program to check if a number is even or odd.
+
+# day 10: Write a program to remove duplicates from a list.
+
+# day 11: Write a program to print the multiplication table of a given number.
